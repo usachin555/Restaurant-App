423a63af006740bc94558b95e6744d7e7a2d2418
usachin555 <sachinsac805@gmail.com>
2025-02-02 15:57:40 +0530

    opened after loong time

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
   }, []);
 
   return(
-    <AnimatePresence mode='wait'>
+    <AnimatePresence mode='wait'> 
     <div className='w-screen h-auto flex flex-col bg-primary'>
       <Header/>
       <main className='mt-14 md:mt-20 px-4 md:px-16 py-4 w-full'>
